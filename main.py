b43aa45d130e0e24141e43b4c0a5fef6aebd3d8d
NahumFGz <36179329+FloresGutierrezNahum@users.noreply.github.com>
2021-07-27 20:48:11 -0500

    Test AWS

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from services.api import app
 
 def main():
     #app.run(port=3000, debug=True)
-    app.run(host="0.0.0.0", port=83, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
 
 if __name__ == '__main__':
     main()
