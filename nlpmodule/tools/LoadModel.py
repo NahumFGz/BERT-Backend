dd0fdd651f2560e5fbd2529d6e74c2033f1f273e
NahumFGz <36179329+FloresGutierrezNahum@users.noreply.github.com>
2021-07-28 17:19:42 -0500

    daemon in linux

--- nlpmodule/tools/LoadModel.py
+++ nlpmodule/tools/LoadModel.py
@@ -1,5 +1,6 @@
 import torch
 
+#def bert_classifier(path='/home/ubuntu/BERT-Backend-Flask/nlpmodule/weights/NewPreprocesing_MDLBETO_BLCnormal_TS0.2_RS2020_epch10_lr5e-05_model1.pth'):
 #def bert_classifier(path='./nlpmodule/weights/Twitter_model1.pth'):
 def bert_classifier(path='./nlpmodule/weights/NewPreprocesing_MDLBETO_BLCnormal_TS0.2_RS2020_epch10_lr5e-05_model1.pth'):
 #def bert_classifier(path='./nlpmodule/weights/AllHaterNetAnd20_24_MDLBETO_BLCnormal_TS0.2_RS2020_epch10_lr5e-05_model1.pth'):
