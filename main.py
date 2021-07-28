ea587c5fc9e720133361bf52e12cc2daf307d246
NahumFGz <36179329+FloresGutierrezNahum@users.noreply.github.com>
2021-07-27 20:18:45 -0500

    test deploy

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from nlpmodule.tools.BertClassifier import BertClassifier
 from services.api import app
 
 def main():
-    app.run(port=3000, debug=True)
+    #app.run(port=3000, debug=True)
+    app.run(host="0.0.0.0", port=83, debug=True)
 
 if __name__ == '__main__':
     main()
