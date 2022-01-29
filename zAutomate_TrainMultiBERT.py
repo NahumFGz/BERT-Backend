43f109889a42a6dfb9c8af456ebfe8984db52fbd
NahumFG <36179329+NahumFGz@users.noreply.github.com>
2022-01-29 14:06:54 -0500

    Update zAutomate_TrainMultiBERT.py

--- zAutomate_TrainMultiBERT.py
+++ zAutomate_TrainMultiBERT.py
@@ -36,7 +36,7 @@ dataset_name  = 'Total_15_19'      # [Total_15_19, Total_20_24, Total_25_29]
 balanceo      = 'normal'            # [normal, over, under]
 test_sz       = 0.1
 learning_rate = 3e-5               # [5e-5, 3e-5, 2e-5]
-num_epochs    = 10                  # [2,3,4,5,6,7,8,9,10]
+num_epochs    = 1                  # [2,3,4,5,6,7,8,9,10]
 rand_state    = 2020
 flg_preproc   = 'processed'          #['original', 'processed']
 
@@ -481,63 +481,62 @@ val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size
 ### 3.1. Create BertClassifier
 """
 
-# Commented out IPython magic to ensure Python compatibility.
-# %%time
-# import torch
-# import torch.nn as nn
-# from transformers import BertModel
-# 
-# # Create the BertClassfier class
-# class BertClassifier(nn.Module):
-#     """Bert Model for Classification Tasks.
-#     """
-#     def __init__(self, freeze_bert=False):
-#         """
-#         @param    bert: a BertModel object
-#         @param    classifier: a torch.nn.Module classifier
-#         @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
-#         """
-#         super(BertClassifier, self).__init__()
-#         # Specify hidden size of BERT, hidden size of our classifier, and number of labels
-#         D_in, H, D_out = 768, 50, 2
-# 
-#         # Instantiate BERT model
-#         self.bert = BertModel.from_pretrained(path_model)
-# 
-#         # Instantiate an one-layer feed-forward classifier
-#         self.classifier = nn.Sequential(
-#             nn.Linear(D_in, H),
-#             nn.ReLU(),
-#             #nn.Dropout(0.5),
-#             nn.Linear(H, D_out)
-#         )
-# 
-#         # Freeze the BERT model
-#         if freeze_bert:
-#             for param in self.bert.parameters():
-#                 param.requires_grad = False
-#         
-#     def forward(self, input_ids, attention_mask):
-#         """
-#         Feed input to BERT and the classifier to compute logits.
-#         @param    input_ids (torch.Tensor): an input tensor with shape (batch_size,
-#                       max_length)
-#         @param    attention_mask (torch.Tensor): a tensor that hold attention mask
-#                       information with shape (batch_size, max_length)
-#         @return   logits (torch.Tensor): an output tensor with shape (batch_size,
-#                       num_labels)
-#         """
-#         # Feed input to BERT
-#         outputs = self.bert(input_ids=input_ids,
-#                             attention_mask=attention_mask)
-#         
-#         # Extract the last hidden state of the token `[CLS]` for classification task
-#         last_hidden_state_cls = outputs[0][:, 0, :]
-# 
-#         # Feed input to classifier to compute logits
-#         logits = self.classifier(last_hidden_state_cls)
-# 
-#         return logits
+#%%time
+import torch
+import torch.nn as nn
+from transformers import BertModel
+
+# Create the BertClassfier class
+class BertClassifier(nn.Module):
+    """Bert Model for Classification Tasks.
+    """
+    def __init__(self, freeze_bert=False):
+        """
+        @param    bert: a BertModel object
+        @param    classifier: a torch.nn.Module classifier
+        @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
+        """
+        super(BertClassifier, self).__init__()
+        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
+        D_in, H, D_out = 768, 50, 2
+
+        # Instantiate BERT model
+        self.bert = BertModel.from_pretrained(path_model)
+
+        # Instantiate an one-layer feed-forward classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            #nn.Dropout(0.5),
+            nn.Linear(H, D_out)
+        )
+
+        # Freeze the BERT model
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+        
+    def forward(self, input_ids, attention_mask):
+        """
+        Feed input to BERT and the classifier to compute logits.
+        @param    input_ids (torch.Tensor): an input tensor with shape (batch_size,
+                      max_length)
+        @param    attention_mask (torch.Tensor): a tensor that hold attention mask
+                      information with shape (batch_size, max_length)
+        @return   logits (torch.Tensor): an output tensor with shape (batch_size,
+                      num_labels)
+        """
+        # Feed input to BERT
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask)
+        
+        # Extract the last hidden state of the token `[CLS]` for classification task
+        last_hidden_state_cls = outputs[0][:, 0, :]
+
+        # Feed input to classifier to compute logits
+        logits = self.classifier(last_hidden_state_cls)
+
+        return logits
 
 """### 3.2. Optimizer & Learning Rate Scheduler
 
@@ -794,16 +793,19 @@ print("Number of tweets predicted non-negative: ", preds_test.sum())
 # Evaluate the Bert classifier
 evaluate_roc(probs_test, y_test,'test')
 
-output = X_test[preds_test==1]
-for i in range(20):
-  print(output[i])
+
+#output = X_test[preds_test==1]
+#for i in range(20):
+#  print(output[i])
 
 """# E - Conclusion
 
 ## Guardar Modelo
 """
 
-PATH_SAVE_MODEL_1 =  result_dir + '_model1.pth' 
-torch.save(bert_classifier, PATH_SAVE_MODEL_1)
+try:
+  PATH_SAVE_MODEL_1 =  result_dir + '_model1.pth' 
+  torch.save(bert_classifier, PATH_SAVE_MODEL_1)
+except:
+  print('Except')
 print(PATH_SAVE_MODEL_1)
-
